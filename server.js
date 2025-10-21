8e28a2f2aa6e6c22c1c28b952e2cd880e235619a
somemorewater <oboyiloejeh@gmail.com>
2025-10-21 23:57:41 +0100

    Created server

--- server.js
+++ server.js
@@ -1,2 +1,8 @@
-const express = require("express");
-const app = express;
+const express = require('express');
+const app = express();
+
+
+
+app.listen(5000, () => {
+    console.log('Server is listening on port 5000...');
+})
