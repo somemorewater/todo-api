3ea50577628d70ac194b93a42b89c62d0245e347
somemorewater <oboyiloejeh@gmail.com>
2025-12-31 17:14:28 +0100

    created the todo model file

---
+++ models/Todo.js
@@ -0,0 +1,8 @@
+const mongoose = require('mongoose');
+
+const todoSchema = new mongoose.Schema({
+  todo: { type: String, required: true },
+  createdAt: { type: Date, default: Date.now },
+});
+
+module.exports = mongoose.model("Todo", todoSchema);
