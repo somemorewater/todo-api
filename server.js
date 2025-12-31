26ab0dd2e43df1140bdd06cf5dfdd96f72937421
somemorewater <oboyiloejeh@gmail.com>
2025-12-31 17:02:16 +0100

    moved the connectDB function

--- server.js
+++ server.js
@@ -1,23 +1,12 @@
 const express = require('express');
 const app = express();
-const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const connectDB = require("./config/db");
 
 dotenv.config();
-
+connectDB();
 const PORT = process.env.PORT || 3000;
 
-const connectDB = async () => {
-  try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("MongoDB Connected");
-  } catch (err) {
-    console.error(err);
-    process.exit(1);
-  }
-};
-
-connectDB();
 
 app.listen(PORT, () => {
     console.log(`Server is listening on port ${PORT}...`);
